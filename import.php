9fbde76d21c73f4f419f9f6f9bdf99dea8bfe270
CJ Holmes <slashingweapon@gmail.com>
2012-01-30 13:00:53 -0800

    use database for import script

--- import.php
+++ import.php
@@ -1,25 +1,44 @@
 <?php
 
-processLines('TWL06.txt', 'parsedwords.txt');
+$dsn = 'mysql:host=localhost;dbname=letteredman';
+$username = 'root';
+$password = '';
 
-function processLines($inFilename, $outFilename) {
-	$outfile = fopen($outFilename, "w");
+$dbh = new PDO($dsn, $username, $password);
+if (!($dbh instanceof PDO)) {
+	echo "No db connection";
+	exit(1);
+}
+
+processLines('TWL06.txt', $dbh);
+
+function processLines($inFilename, $dbConn) {
+	$query = "INSERT INTO words (len, letters, word) VALUES (?, ?, ?)";
+	$statement = $dbConn->prepare($query);
+	$linecount = 0;
 	
 	$lines = file($inFilename);
 	foreach($lines as $oneLine) {
 		if (!empty($oneLine)) {
 			$oneLine = trim($oneLine);
+			$oneLine = strtoupper($oneLine);
 			$sortWord = lettersort($oneLine);
 			$length = strlen($sortWord);
-			fwrite($outfile, "$length\t$sortWord\t$oneLine\n");
+			
+			if (! $statement->execute(array($length, $sortWord, $oneLine))) {
+				echo "Insert failed\n";
+				exit();
+			}
+			
+			$linecount++;
+			if (!($linecount % 1000))
+				echo "Added $linecount words\n";
 		}
 	}
 }
 
 function lettersort($word) {
 	$letters = array();
-	$word = trim($word);
-	$word = strtoupper($word);
 	$count = strlen($word);
 	
 	for($idx=0; $idx<$count; $idx++)
