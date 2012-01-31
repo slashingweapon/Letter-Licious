2e3a3387b789755ec000347075720f6c6c35a78a
CJ Holmes <slashingweapon@gmail.com>
2012-01-31 13:38:52 -0500

    Tear it all apart to make it AJAXy

--- application/controllers/letters.php
+++ application/controllers/letters.php
@@ -1,14 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Letters extends CI_Controller {
+require_once("json_controller.php");
+
+class Letters extends Json_Controller {
 
 	function __construct() {
 		parent::__construct();
 		$this->load->helper('letter');
+		$this->load->library('user_agent');
 		$this->load->model('words_model', '', true);
 	}
 	
 	public function index() {
+//		$page = "desktop.html";
+//		if ($this->agent->is_mobile())
+			$page = "mobile.html";
+		
+		$file = APPPATH . "../public/$page";
+		if (file_exists($file))
+			readfile($file);
+		else
+			show_error("Could not locate $page ($file)",500);
+	}
+	
+	public function noindex() {
 		$search = $this->input->post("search");
 		$words = array();
 		$letters = false;
@@ -19,13 +34,41 @@ class Letters extends CI_Controller {
 			list($letters, $unique_letters) = processLetters($search);
 		
 		if (!empty($unique_letters))
-			$words = $this->words_model->getWordsForUniqueLetters($unique_letters);
+			$words = $this->words_model->findWordsByEnumeratedLetters($unique_letters);
 		
 		$this->template->assign(array(
 			'search' => $search,
 			'words' => $words,
 			'letters' => $letters,
 			'unique_letters' => $unique_letters,
+		) );
+		$this->template->display('letters.tpl');
+	}
+	
+	public function qWithoutU() {
+		$words = $this->words_model->findQWithoutU();
+		
+		$this->template->assign(array(
+			'search' => false,
+			'words' => $words,
+		) );
+		$this->template->display('letters.tpl');
+	}
+	
+	public function two() {
+		$this->byLength(2);
+	}
+	
+	public function three() {
+		$this->byLength(3);
+	}
+	
+	private function byLength($len) {
+		$words = $this->words_model->findWordsOfLength($len);
+		
+		$this->template->assign(array(
+			'search' => false,
+			'words' => $words,
 		) );
 		$this->template->display('letters.tpl');
 	}
--- application/models/words_model.php
+++ application/models/words_model.php
@@ -9,16 +9,62 @@ class Words_model extends CI_Model {
 	 *
 	 *	Returns an array of words.  Returns an empty array on failure.
 	 */
-	public function getWordsForUniqueLetters($letters) {
+	public function findWordsByEnumeratedLetters($letters) {
 		$retval = array();
 		$letterStuff = implode(',', $letters);
 		
-		$query = $this->db->query("select word from words where letters <@ '\{$letterStuff}' order by len DESC, word");
-		foreach($query->result() as $row) {
-			$retval[] = $row->word;
-		}
+		$query = $this->db->from('words')
+			->select('trim(from word) as trimword', false)
+			->where("letters <@ '\{$letterStuff}'")
+			->order_by('len', "desc")
+			->order_by("trimword") 
+			->get();
 		
+		foreach($query->result() as $row)
+			$retval[] = $row->trimword;
+			
 		return $retval;
 	}
+	
+	/**
+	 *	Returns a list of all words of a given length.  This is meant to be used only with values
+	 *	2 or 3.  Using bigger numbers could be detrimental to your database performance.
+	 *
+	 *	@param int len The number of letters in the words.
+	 *	@return array An array of words
+	 */
+	 public function findWordsOfLength($len) {
+	 	$retval = array();
+	 	
+	 	$query = $this->db->from('words')
+	 		->select('trim(from word) as trimword', false)
+	 		->where('len', $len)
+	 		->order_by('trimword')
+	 		->get();
+	 	
+	 	foreach($query->result() as $row)
+	 		$retval[] = $row->trimword;
+	 		
+	 	return $retval;
+	 }
+	 
+	 /**
+	  *	Finds all words that contain Q but do not include the letter U.
+	  *
+	  *	select trim(from word) as trimword from words where '{Q0}' <@ letters and NOT '{U0}' <@ letters;
+	  *
+	  *	@return array An array of strings
+	  */
+	 public function findQWithoutU() {
+	 	$retval = array();
+	 	
+	 	$query = $this->db->query("select trim(from word) as trimword from words where '{Q0}' <@ letters and NOT '{U0}' <@ letters");
+	 	
+	 	foreach($query->result() as $row)
+	 		$retval[] = $row->trimword;
+	 		
+	 	return $retval;
+	 }
+	 
 }
 
