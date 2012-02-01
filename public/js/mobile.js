1a9473016d98aca667653dd79638ca2abd854643
CJ Holmes <slashingweapon@gmail.com>
2012-01-31 22:16:19 -0500

    Fix the qWithoutU list; Enable rotation

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -11,24 +11,30 @@
  *
  */
 
+var currentWords = [];
+var currentColumns = 3;
 var api = new cww_api();
 api.debug = true;
 
-function fillWordTable(words) {
+function setWords(words) {
+	currentWords = words;
+	renderWordTable();
+}
+
+function renderWordTable() {
 	var html = "";
-	var columns = 3;
 	
-	if (words.length > 0) {
-		for(idx=0; idx<words.length; idx++) {
-			if (idx % columns == 0) {
+	if (currentWords.length > 0) {
+		for(idx=0; idx<currentWords.length; idx++) {
+			if (idx % currentColumns == 0) {
 				html += "<tr>";
 			}
-			html += "<td>" + words[idx] + "</td>";
-			if (idx % columns == columns - 1) {
+			html += "<td>" + currentWords[idx] + "</td>";
+			if (idx % currentColumns == currentColumns - 1) {
 				html += "</tr>"
 			}
 		}
-		if (idx % columns != columns - 1) {
+		if (idx % currentColumns != currentColumns - 1) {
 			html += "</tr>";
 		}
 	} else {
@@ -39,15 +45,35 @@ function fillWordTable(words) {
 }
 
 $(document).ready(function() {
+	// Check for orientation changes.
+	$("body").on("orientationchange", function() {
+		switch(window.orientation) {
+			case 90:
+			case -90:
+				currentColumns = 4;
+				break;
+			case 0:
+			case 180:
+			default:
+				currentColumns = 3;
+				break;
+		}
+		renderWordTable();
+	});
+	
+	// Hook up the search box
 	$("#searchBtn").click(function(evt) {
 		evt.preventDefault();
 		var searchLetters = $("#searchLetters").val();
-		api.search(searchLetters, fillWordTable);
+		$("#wordTable caption").html("Words using " + searchLetters);
+		api.search(searchLetters, setWords);
 	});
+
 	// Hook up our list links
 	$("#2letter,#greek,#qwithoutu").click(function(evt) {
 		evt.preventDefault();
 		$("#wordTable caption").html(evt.target.title);
-		api.list(evt.target.id, fillWordTable);
+		api.list(evt.target.id, setWords);
 	});
+	
 });
