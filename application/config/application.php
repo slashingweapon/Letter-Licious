e2706db0ee3a6a7cbd77b4ec6e22b51447ef17cf
CJ Holmes <slashingweapon@gmail.com>
2012-02-02 00:08:10 -0500

    Issue #5: iPod icon

--- application/config/application.php
+++ application/config/application.php
@@ -1,10 +1,11 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['version'] = "r3";
+$config['version'] = "r3.1";
 
 $config['manifest'] = array(
 	"/js/jquery-1.7.1.min.js",
 	"/js/cww_api.js",
 	"/js/mobile.js",
 	"/css/small-device.css",
+	"/word.png",
 );
