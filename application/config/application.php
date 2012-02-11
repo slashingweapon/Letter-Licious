46618b90bd4ebb98ba3c38f95f5daecc9d09f78d
CJ Holmes <slashingweapon@gmail.com>
2012-02-10 22:50:37 -0500

    Bump version number

--- application/config/application.php
+++ application/config/application.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['version'] = "r4.2";
+$config['version'] = "r5";
 
 $config['clientCache'] = 43200;	// 12 hours (in seconds) before a date check
 
